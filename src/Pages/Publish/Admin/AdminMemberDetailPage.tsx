ba95fe2cfead978c860dc25d43125abb64a7fa9f
psmever <psmever@gmail.com>
2023-12-14 16:32:40 +0900

    build: 항목 추가

--- src/Pages/Publish/Admin/AdminMemberDetailPage.tsx
+++ src/Pages/Publish/Admin/AdminMemberDetailPage.tsx
@@ -7,8 +7,8 @@ const AdminMemberDetailPage = () => {
         <div className="min-h-screen h-screen flex flex-col dark:bg-gray-700">
             <AdminHeader />
 
-            <div className="flex-1 flex flex-row h-screen overflow-y-hidden">
-                <main className="flex-1 h-screen bg-indigo-50 border-l border-r border-indigo-300 text-xs overflow-y-auto dark:bg-gray-700">
+            <div className="flex-1 flex flex-row overflow-y-hidden">
+                <main className="flex-1 bg-indigo-50 border-l border-r border-indigo-300 text-xs overflow-y-auto dark:bg-gray-700">
                     {/* Contents Section */}
                     <div className="flex w-full h-full items-baseline justify-center">
                         <div className="flex flex-col w-full max-w-3xl justify-center p-5 shadow-2xl">
@@ -23,12 +23,53 @@ const AdminMemberDetailPage = () => {
                                         <div className="grid gap-6 mb-6 md:grid-cols-2">
                                             <div>
                                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                    회원 번호
+                                                </label>
+                                                <input
+                                                    type="number"
+                                                    readOnly={true}
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    placeholder="1111"
+                                                />
+                                            </div>
+                                            <div>
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                    가입경로
+                                                </label>
+                                                <input
+                                                    type="text"
+                                                    readOnly={true}
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    placeholder="Web"
+                                                />
+                                            </div>
+                                            <div>
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">종류</label>
+                                                <input
+                                                    type="text"
+                                                    readOnly={true}
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    placeholder="카카오톡"
+                                                />
+                                            </div>
+                                            <div>
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">레벨</label>
+                                                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                    <option value="">선택</option>
+                                                    <option value="0001">일반</option>
+                                                    <option value="0002">관리자</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="grid gap-6 mb-6 md:grid-cols-2">
+                                            <div>
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                                     이메일
                                                 </label>
                                                 <input
                                                     type="email"
                                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                    placeholder="John"
+                                                    placeholder="xxxx@xxx.com"
                                                 />
                                             </div>
                                             <div>
@@ -46,7 +87,7 @@ const AdminMemberDetailPage = () => {
                                                 <input
                                                     type="text"
                                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                    placeholder="Flowbite"
+                                                    placeholder="이름"
                                                 />
                                             </div>
                                             <div>
@@ -60,7 +101,7 @@ const AdminMemberDetailPage = () => {
                                         </div>
                                         <div className="mb-6">
                                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">생년월일</label>
-                                            <div className="flex gap-6 mb-6 md:grid-cols-1">
+                                            <div className="flex gap-6 md:grid-cols-1">
                                                 <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                                     <option value="">선택</option>
                                                     <option value="0001">1990</option>
@@ -76,6 +117,58 @@ const AdminMemberDetailPage = () => {
                                                     <option value="0001">01</option>
                                                     <option value="0002">02</option>
                                                 </select>
+                                            </div>
+                                        </div>
+                                        <div className="grid gap-6 mb-6 md:grid-cols-2">
+                                            <div>
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                    좋아하는 스타일
+                                                </label>
+                                                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                    <option value="">선택</option>
+                                                    <option value="0001">공격</option>
+                                                    <option value="0002">벨런스</option>
+                                                    <option value="0003">스타일</option>
+                                                </select>
+                                            </div>
+                                            <div>
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                    자신 있는 능력
+                                                </label>
+                                                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                    <option value="">선택</option>
+                                                    <option value="0001">슛</option>
+                                                    <option value="0002">패스</option>
+                                                    <option value="0002">드리블</option>
+                                                    <option value="0002">체력</option>
+                                                    <option value="0002">스피드</option>
+                                                    <option value="0002">피지컬</option>
+                                                    <option value="0002">골키퍼</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="grid gap-6 mb-6 md:grid-cols-2">
+                                            <div>
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                    포인트
+                                                </label>
+                                                <input
+                                                    type="number"
+                                                    readOnly={true}
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    placeholder="12,973"
+                                                />
+                                            </div>
+                                            <div>
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                    마지막 로그인
+                                                </label>
+                                                <input
+                                                    type="text"
+                                                    readOnly={true}
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                    placeholder="2023.12.17 08:00"
+                                                />
                                             </div>
                                         </div>
                                         <div className="flex flex-nowrap w-full gap-8">
