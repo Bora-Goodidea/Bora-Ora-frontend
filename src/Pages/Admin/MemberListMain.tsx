6a51a141d2617e17fc9863255f88ee6708e4b20c
서보문 <gloriogo@gmail.com>
2023-12-19 18:13:26 +0900

    build: 관리자-회원관리-회원목록 화면 작업

--- src/Pages/Admin/MemberListMain.tsx
+++ src/Pages/Admin/MemberListMain.tsx
@@ -1,109 +1,86 @@
 import AdminStyles from '@Style/AdminStyles';
 import React from 'react';
-import AdminHeader from './AdminHeader';
-import AdminFooter from './AdminFooter';
-import AdminNav from './AdminNav';
 import lodash from 'lodash';
+import AdminLayout from './AdminLayout';
 
-const { AdminLayout } = AdminStyles.AdminLayoutStyle;
-const { ContentContainer, MainContainer } = AdminStyles.ContentWrapperStyle;
+const {
+    ContentContainer,
+    ContentHeadContainer,
+    ContentHeadInputWrapper,
+    ContentHeadInputContainer,
+    ContentHeadCheckbox,
+    ContentHeadCheckboxLabel,
+    ContentHeadTitle,
+    ContentBodyWrapper,
+    ContentBodyContainer,
+    ContentBodyInputWrapper,
+    ContentBodyInputContainer,
+    ContentBodyCheckbox,
+    ContentBodyText,
+    ContentBodyButtons,
+    ContentBodyButtonBlue,
+    ContentBodyButtonRed,
+} = AdminStyles.ContentSectionMemberListStyle;
 
 const MemberListMain = () => {
     return (
         <AdminLayout>
-            <AdminHeader />
+            {/* Contents Section */}
             <ContentContainer>
-                <MainContainer>
-                    {/* Contents Section */}
-                    <div className="relative flex-col">
-                        <div className="flex flex-nowrap w-full h-8 justify-center text-xs text-gray-700 bg-gray-500 dark:bg-gray-700 dark:text-gray-100 sticky top-0 z-50">
-                            <div className="flex w-1/12 items-center justify-center border-l border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                <div className="flex items-center">
-                                    <input
-                                        id="checkbox-all-search"
-                                        type="checkbox"
-                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-500 dark:border-gray-600"
-                                    />
-                                    <label htmlFor="checkbox-all-search" className="sr-only">
-                                        checkbox
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                이름
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                이메일
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                레벨
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                생년월일
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                성별
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                가입경로
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                상태
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                등록일
-                            </div>
-                            <div className="flex w-1/12 items-center justify-center border-r border-b dark:bg-gray-500 dark:border-gray-600">
-                                관리
-                            </div>
-                        </div>
-                        <div className="flex flex-col justify-center text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            {lodash.map(
-                                Array.from({ length: 30 }, (v, i) => i),
-                                () => {
-                                    return (
-                                        <div className="flex flex-nowrap w-full h-8 justify-center text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 cursor-pointer">
-                                            <div className="flex w-1/12 items-center justify-center border-l border-r border-b">
-                                                <div className="flex items-center">
-                                                    <input
-                                                        id="checkbox-all-search"
-                                                        type="checkbox"
-                                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                                    />
-                                                    <label htmlFor="checkbox-all-search" className="sr-only">
-                                                        checkbox
-                                                    </label>
-                                                </div>
-                                            </div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">서보문</div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">test@test.com</div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">관리자</div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">78.10.22</div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">남성</div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">카카오</div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">인증완료</div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">2023.12.01</div>
-                                            <div className="flex w-1/12 items-center justify-center border-r border-b">
-                                                <div className="flex gap-1">
-                                                    <button className="bg-blue-500 text-white font-bold py-1 px-3 rounded hover:bg-blue-700 dark:bg-gray-300 dark:border-gray-600 dark:hover:bg-blue-700">
-                                                        보기
-                                                    </button>
-                                                    <button className="bg-blue-500 text-white font-bold py-1 px-3 rounded hover:bg-blue-700 dark:bg-red-300 dark:border-gray-600 dark:hover:bg-red-700">
-                                                        블럭처리
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    );
-                                }
-                            )}
-                        </div>
-                    </div>
-                    {/* Contents Section */}
-                </MainContainer>
+                <ContentHeadContainer>
+                    <ContentHeadInputWrapper>
+                        <ContentHeadInputContainer>
+                            <ContentHeadCheckbox id="checkbox-all-search" type="checkbox" />
+                            <ContentHeadCheckboxLabel htmlFor="checkbox-all-search" className="sr-only">
+                                checkbox
+                            </ContentHeadCheckboxLabel>
+                        </ContentHeadInputContainer>
+                    </ContentHeadInputWrapper>
+                    <ContentHeadTitle>이름</ContentHeadTitle>
+                    <ContentHeadTitle>이메일</ContentHeadTitle>
+                    <ContentHeadTitle>레벨</ContentHeadTitle>
+                    <ContentHeadTitle>생년월일</ContentHeadTitle>
+                    <ContentHeadTitle>성별</ContentHeadTitle>
+                    <ContentHeadTitle>가입경로</ContentHeadTitle>
+                    <ContentHeadTitle>상태</ContentHeadTitle>
+                    <ContentHeadTitle>등록일</ContentHeadTitle>
+                    <ContentHeadTitle>관리</ContentHeadTitle>
+                </ContentHeadContainer>
+                <ContentBodyWrapper>
+                    {lodash.map(
+                        Array.from({ length: 30 }, (v, i) => i),
+                        () => {
+                            return (
+                                <ContentBodyContainer>
+                                    <ContentBodyInputWrapper>
+                                        <ContentBodyInputContainer>
+                                            <ContentBodyCheckbox id="checkbox-all-search" type="checkbox" />
+                                            <ContentHeadCheckboxLabel htmlFor="checkbox-all-search" className="sr-only">
+                                                checkbox
+                                            </ContentHeadCheckboxLabel>
+                                        </ContentBodyInputContainer>
+                                    </ContentBodyInputWrapper>
+                                    <ContentBodyText>서보문</ContentBodyText>
+                                    <ContentBodyText>test@test.com</ContentBodyText>
+                                    <ContentBodyText>관리자</ContentBodyText>
+                                    <ContentBodyText>78.10.22</ContentBodyText>
+                                    <ContentBodyText>남성</ContentBodyText>
+                                    <ContentBodyText>카카오</ContentBodyText>
+                                    <ContentBodyText>인증완료</ContentBodyText>
+                                    <ContentBodyText>2023.12.01</ContentBodyText>
+                                    <ContentBodyText>
+                                        <ContentBodyButtons>
+                                            <ContentBodyButtonBlue>보기</ContentBodyButtonBlue>
+                                            <ContentBodyButtonRed>블럭처리</ContentBodyButtonRed>
+                                        </ContentBodyButtons>
+                                    </ContentBodyText>
+                                </ContentBodyContainer>
+                            );
+                        }
+                    )}
+                </ContentBodyWrapper>
             </ContentContainer>
-            <AdminNav />
-            <AdminFooter />
+            {/* Contents Section */}
         </AdminLayout>
     );
 };
